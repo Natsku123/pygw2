2c38b1f4fa9a5922ba098c8669f142297dba4aee
Max Mecklin <max@meckl.in>
2021-04-19 11:31:34 +0300

    Base for misc api endpoints

---
+++ pygw2/api/misc.py
@@ -0,0 +1,28 @@
+from ..utils import endpoint
+
+
+class MiscellaneousApi:
+    def __init__(self):
+        self.api_key: str = ""
+
+    def setup(self, api_key: str):
+        self.api_key = api_key
+
+    @endpoint("/v2/build")
+    async def build(self, *, data):
+        """
+        Get current build id from API
+        :param data: Data from wrapper
+        :return:
+        """
+        return data
+
+    @endpoint("/v2/colors", has_ids=True)
+    async def colors(self, *, data, ids: list = None):
+        """
+        Get colors from API by list of IDs or one ID.
+        :param data: Data from wrapper
+        :param ids: List of IDs
+        :return: list
+        """
+        return data
