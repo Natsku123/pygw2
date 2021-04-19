da8665846bfed533a08a987a62be355319868af3
Max Mecklin <max@meckl.in>
2021-04-19 11:39:28 +0300

    Currency endpoint

--- pygw2/api/misc.py
+++ pygw2/api/misc.py
@@ -1,5 +1,5 @@
 from ..utils import endpoint, object_parse
-from ..core.models.misc import Color
+from ..core.models.misc import Color, Currency
 
 
 class MiscellaneousApi:
@@ -33,3 +33,19 @@ class MiscellaneousApi:
 
         # Return object(s)
         return object_parse(data, Color)
+
+    @endpoint("/v2/currencies", has_ids=True)
+    async def currencies(self, *, data, ids: list = None):
+        """
+        Get currencies from API by list of IDs or one ID.
+        :param data: Data from wrapper
+        :param ids: List of IDs
+        :return: list
+        """
+
+        # Return ids
+        if ids is None:
+            return data
+
+        # Return object(s)
+        return object_parse(data, Currency)
